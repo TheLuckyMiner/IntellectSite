454ea135ff20ad6675c2c9177340eba6e7280e40
TheLuckyMiner <121936425+TheLuckyMiner@users.noreply.github.com>
2023-01-09 18:32:22 +0300

    Add slider

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+    $(".facts__slider").slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000
+
+    })
+});
